736f4b21588fe0ff3c3d131ea8e94ac65d53b61b
0xZisu <lazy1523@outlook.com>
2024-11-13 04:11:20 +0800

    init

--- src/constants.ts
+++ src/constants.ts
@@ -4,11 +4,11 @@ import JSBI from 'jsbi'
 /**
  * @deprecated use FACTORY_ADDRESS_MAP instead
  */
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x9260bb1a28A1fD9f8DbD4386577003B51bb07FA6'
 
 export const FACTORY_ADDRESS_MAP: { [chainId: number]: string } = V2_FACTORY_ADDRESSES
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export const INIT_CODE_HASH = '0x729407b232da8c9e66bf9687d2f09ec5f9d032507e9321909ca4c651a5ed314d'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
